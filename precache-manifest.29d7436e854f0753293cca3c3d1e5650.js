ef155aec102d109dc3d69b6977e05a2ba8b29e93
Yun-Chamnab <chamnab.yun@student.niptict.edu.kh>
2021-12-25 11:26:25 +0700

    Updates

---
+++ precache-manifest.29d7436e854f0753293cca3c3d1e5650.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4857b08c0f1ee63d59cad331261dab",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "72d94111df16fd92da62",
+    "url": "/developerFolio/static/css/3.5a09a2c6.chunk.css"
+  },
+  {
+    "revision": "408d23d1f49854955d28",
+    "url": "/developerFolio/static/css/4.ca6b759e.chunk.css"
+  },
+  {
+    "revision": "850127060f65c3564438",
+    "url": "/developerFolio/static/css/main.9aff2edd.chunk.css"
+  },
+  {
+    "revision": "0481b37f35b9645aa5af",
+    "url": "/developerFolio/static/js/2.eeb2dec1.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.eeb2dec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d94111df16fd92da62",
+    "url": "/developerFolio/static/js/3.559b0ac1.chunk.js"
+  },
+  {
+    "revision": "408d23d1f49854955d28",
+    "url": "/developerFolio/static/js/4.960a9be1.chunk.js"
+  },
+  {
+    "revision": "850127060f65c3564438",
+    "url": "/developerFolio/static/js/main.91634b5b.chunk.js"
+  },
+  {
+    "revision": "133da56d47569ec1978e",
+    "url": "/developerFolio/static/js/runtime-main.1caf706f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "74564542326de164b2d6398ceb12a2f9",
+    "url": "/developerFolio/static/media/cadt.74564542.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/developerFolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "df3d38538ba5534f4a8e4622d7aada74",
+    "url": "/developerFolio/static/media/scoring_logo.df3d3853.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/developerFolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "5ab64b6ad481c827941562f0eb6ca995",
+    "url": "/developerFolio/static/media/watchfree_logo.5ab64b6a.webp"
+  }
+]);
